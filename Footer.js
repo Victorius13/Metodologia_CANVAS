64494125facbc5de15f1f088a1a04218eb8dde69
Victor Manuel Jerez Lara <jerezv313@gmail.com>
2025-03-28 17:52:44 -0400

    Delete Footer.js

--- Footer.js
+++
@@ -1,8 +0,0 @@
-function actualizarHora() {
-  const ahora = new Date();
-  const hora = ahora.toLocaleTimeString();
-  document.getElementById("hora").textContent = hora;
-}
-
-actualizarHora();
-setInterval(actualizarHora, 1000);
